c2f2840b4873ab467b373950815a3fabf09e0d93
ChuanQin <callforsky@gmail.com>
2018-09-14 16:06:51 -0400

    Set up Reducers more

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,1 +1,11 @@
 // exports our invocation to combine reducers
+import { combineReducers } from 'redux'
+import authedUser from './authedUser'
+import users from './users'
+import tweets from './tweets'
+
+export default combineReducers({
+  authedUser,
+  users,
+  tweets
+})
